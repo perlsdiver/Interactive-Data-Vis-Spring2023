7b43a78cb98ca322717e3343ca498f524a8dd3ca
perlsdiver <92558766+perlsdiver@users.noreply.github.com>
2023-02-12 14:17:19 -0500

    working on homework

--- 1_1_getting_started/NOTES.md
+++ 1_1_getting_started/NOTES.md
@@ -4,8 +4,10 @@
 INSTRUCTIONS:
 Use this markdown file to keep track of open questions/challenges from this week's assignment.
 - What did you have trouble solving?
-## It took me a moment to get oriented and figure out the interface of doing it all. I have trouble with translating instructions to mental models and tend to need to learn through repeated process exposure (and often, through things failing/debugging).
+
+It took me a moment to get oriented and figure out the interface of doing it all. I have trouble with translating instructions to mental models and tend to need to learn through repeated process exposure (and often, through things failing/debugging).
 - What went easier than expected?
+Getting back into some of the basic flow of writing code. It's been a little while.
 - What, if anything, is currently blocking you?
 
 Sometimes it helps to formulate what you understood and where you got stuck in order to move forward. Feel free to include `code snippets`, `screenshots`, and `error message text` here as well.
--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,6 +1,43 @@
 console.log('hello world');
 
-// conole.log(document)
+// console.log(document)
 // console.log(window)
 
-const input = document
+const input = document.getElementById("name-input")
+// console.log(input)
+// console.log(document.getElementById("name-input"))
+const updateName = () => {
+    // console.log('in update function')
+    const userName = input.value;
+    window.alert('Hello, welcome to class ${userName}')
+
+}
+
+// updateName();
+
+let changeable = true
+let constant = true
+let counter = 0
+
+function change() {
+    changeable = false;
+    const constant = false;
+    // console.log(changeable, constant)
+    counter = counter + 1
+}
+
+// change();
+// change();
+// change
+// change
+// console.log('counter', counter)
+// console.log(changeable, constant)
+
+var count = 0;
+        var btn = document.getElementById("btn");
+        var disp = document.getElementById("display");
+          
+        btn.onclick = function () {
+            count++;
+            disp.innerHTML = count;
+        }
